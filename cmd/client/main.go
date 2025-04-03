1e0cd5389471e748385f6c077c780b856a29099a
magicznykacpur <kafejek@gmail.com>
2025-04-03 15:27:08 +0200

    make single client be able to connect and bind his exchange

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const guestUrl = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	log.Println("Connecting to rabbitMq server...")
+
+	conn, err := amqp.Dial(guestUrl)
+	if err != nil {
+		log.Printf("couldnt dial connection with %s: %v\n", guestUrl, err)
+	}
+	defer conn.Close()
+
+	log.Println("Connection to rabbitMq server successfull!")
+	log.Println("Starting Peril client...")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Printf("couldn't get client welcome message: %v", err)
+	}
+
+	pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey,
+		0,
+	)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	log.Println("Peril client gracefully stopped.")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -22,3 +22,46 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		amqp.Publishing{ContentType: "application/json", Body: bytes},
 	)
 }
+
+type queueType int
+
+const (
+	transient queueType = iota
+	nonTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType queueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	var queue amqp.Queue
+
+	if simpleQueueType == transient {
+		queue, err = channel.QueueDeclare(queueName, true, true, true, false, nil)
+		if err != nil {
+			return nil, amqp.Queue{}, err
+		}
+	}
+
+	if simpleQueueType == nonTransient {
+		queue, err = channel.QueueDeclare(queueName, false, false, false, false, nil)
+		if err != nil {
+			return nil, amqp.Queue{}, err
+		}
+	}
+
+	err = channel.QueueBind(queue.Name, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return channel, queue, nil
+}
