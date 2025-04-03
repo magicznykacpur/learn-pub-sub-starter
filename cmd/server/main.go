73f3d85a61b9b00bb913b24543906eaa8ce04411
magicznykacpur <kafejek@gmail.com>
2025-04-03 13:44:58 +0200

    connect to local rabbitmq, handle exit gracefully

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const guestUrl = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	log.Println("Connecting to rabbitMq server...")
+
+	conn, err := amqp.Dial(guestUrl)
+	if err != nil {
+		log.Printf("couldnt dial connection with %s: %v\n", guestUrl, err)
+	}
+	defer conn.Close()
+
+	log.Println("Connection to rabbitMq server successfull!")
+	log.Println("Starting Peril server...")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Peril server gracefully stopped.")
 }
